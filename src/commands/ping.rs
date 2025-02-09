c39e0df31904804c8cfddfcec1eeb3873ba7c67a
Lavion <ilavion4u@gmail.com>
2025-02-10 00:48:56 +0100

    Simplify ping command: remove unused imports and clean up code

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,9 +1,5 @@
 // Filename: commands/ping.rs
 // A simple ping command
-
-use crate::Error;
-use poise::serenity_prelude::Context;
-
 /// Responds with "Pong!"
 #[poise::command(slash_command)]
 pub async fn ping(ctx: poise::Context<'_, crate::Data, crate::Error>) -> Result<(), crate::Error> {
